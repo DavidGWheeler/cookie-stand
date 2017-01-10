73376e468b429e02735493622f2c439ca20e64d5
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-09 16:32:53 -0800

    work in progress on for loop

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var opHours = [6,20];
+
+var firstAndPike = {
+  locationName: '1st & Pike',
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgPerSale: 6.3,
+};
+
+var seaTacAirport = {
+  locationName: 'SeaTac Airport',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgPerSale: 1.2,
+};
+
+var seaCent = {
+  locationName: 'Seattle Center',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgPerSale: 1.7,
+};
+
+var capHill = {
+  locationName: 'Capitol Hill',
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgPerSale: 2.3,
+};
+
+var alki = {
+  locationName: 'Alki',
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgPerSale: 4.6,
+};
