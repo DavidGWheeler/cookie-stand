20aee1b099aed86899734c7203515f883f8eecab
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-01-13 08:49:07 -0800

    this is broken code for dailyCookies

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 'use strict';
 
 //constructor builds out a store with a location name and min/max customers per hour.
-function CookieStore (locationName, minCust, maxCust, avgPerSale, opHours){
+function CookieStore (locationName, minCust, maxCust, avgPerSale, opHours, sold){
   this.locationName = locationName;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgPerSale = avgPerSale;
   this.opHours = opHours;
+  this.sold = sold;
 }
 
 CookieStore.prototype.hourlyCustVolEst = function(){
   return (Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust));
 };
 
+//calculate the average number of cookies sold per hour and return hourlyCookies
 CookieStore.prototype.hourlySoldAvg = function(){
   var hourlyCookies = [];
   for (var i = 0; i < this.opHours.length; i++) {
@@ -28,10 +30,20 @@ CookieStore.prototype.hourlyCookies = function(){
   var runningTotal = 0;
   for (var i = 0; i < this.hourlyCookies.length; i++) {
     runningTotal = this.hourlyCookies[i] + runningTotal;
-    console.log(runningTotal + ': = dailyCookies sold loop');
+    // console.log(runningTotal + ': = hourlyCookies sold loop');
   }
-  // console.log(runningTotal + ': = dailyCookiesTotal');
+  // console.log(runningTotal + ': = hourlyCookiesTotal');
   return runningTotal;
+};
+
+CookieStore.prototype.dailyCookies = function(){
+  var runningDTotal = 0;
+  for (var i = 0; i < this.hourlyCookies.length; i++) {
+    runningDTotal = this.hourlyCookies[i] + runningDTotal;
+    console.log(runningDTotal + ': = dailyCookiesTotal sold loop');
+  }
+  console.log(runningDTotal + ': = dailyCookiesTotal');
+  return runningDTotal;
 };
 
 // creates a table using object data
@@ -40,13 +52,21 @@ function salesTable(object){
   for(var i = 0; i < object.length; i++){
     var tableRow = document.createElement('tr');
     tableRow.textContent = object[i].locationName;
-    // console.log(object.locationName);
-    // console.log(object);
+    console.log(object[i].locationName);
+    console.log(object);
+
     var hourlyCookies = object[i].hourlySoldAvg();
     for(var j = 0; j < object[i].hourlySoldAvg().length; j++){
       var tableData = document.createElement('td');
       tableData.textContent = hourlyCookies[j];
       tableRow.appendChild(tableData);
+
+      var dailyCookies = object[i].dailyCookies();
+      for (var k = 0; k < obect[i].dailyCookies().length; i++) {
+        var tableFoot = document.createElement('tfoot');
+        tableFoot.textContent = dailyCookies[k];
+        tableRow.appendChild(tableFoot);
+      }
     }
     tableEl.appendChild(tableRow);
   }
@@ -84,21 +104,5 @@ newStoreFormEl.addEventListener('submit', function(event){
 //I don't like that this is a global variable. todo: make this a part of a constructor.
 var constructorArray = [firstAndPike, seaTacAirport, seaCent, capHill, alki];
 
-//global function  I don't want it to be
+//global function  Do I want it to be?
 salesTable(constructorArray);
-
-// //Write hourlyCookies sales to the DOM
-// CookieStore.prototype.hourlySalesWrite = function() {
-//   var todaySold = document.getElementById('sales-list');
-//   for (var i = 0; i < this.hourlyCookies.length; i++){
-//     var rowEl = document.createElement('li');
-//     rowEl.textContent = this.opHours[i] + ': ' + this.hourlyCookies[i] + 'cookies.';
-//     todaySold.appendChild(rowEl);
-// };
-//
-// //Write daily cookie sales to the DOM
-// CookieStore.prototype.dailySalesWrite = function(){
-//   document.createElement('li');
-//   dailySalesWrite.textContent = 'Total: ' + this.dailyCookiesTotal();
-//   todaySold.appendChild(this.dailySalesWrite);
-// };
